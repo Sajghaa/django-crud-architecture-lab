60b2690f2e190d160340ce260c9c0aa70097b4f1
Serge <sajghaa@gmail.com>
2026-02-20 20:38:04 +0200

    refined  base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,3 +1,62 @@
 from decouple import config
+from pathlib import Path
 
-SECRET_KEY= config("SECRET_KEY")
+# ---------------------------
+# BASE_DIR for paths
+# ---------------------------
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# ---------------------------
+# SECRET KEY (externalized)
+# ---------------------------
+SECRET_KEY = config("SECRET_KEY")
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # future apps will go here
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],  # our global templates
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+# ---------------------------
+# STATIC FILES (shared for all environments)
+# ---------------------------
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
