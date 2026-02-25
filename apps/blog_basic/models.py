0c40d1b3cc978e0d86afaad8dd1a6debc0d4b611
Serge <sajghaa@gmail.com>
2026-02-25 23:31:27 +0200

    git updated models.py

--- apps/blog_basic/models.py
+++ apps/blog_basic/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog_basic:post_detail", kwargs={"pk": self.pk})
