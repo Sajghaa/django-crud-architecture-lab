e50b44d401f4ac7c2cd8481a45c470ce95c16bda
Serge <sajghaa@gmail.com>
2026-02-26 21:56:04 +0200

    apps.py updated

--- apps/blog_basic/apps.py
+++ apps/blog_basic/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+name = "apps.blog_basic"
 class BlogBasicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.blog_basic'
