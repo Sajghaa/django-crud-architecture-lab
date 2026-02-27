a375ea87490e781412bf0d297c0a7682ad8bf417
Serge <sajghaa@gmail.com>
2026-02-27 22:23:38 +0200

    config/urls.py updated

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("blog/", include("apps.blog_basic.urls", namespace="blog_basic")),
+    path("", include("apps.blog_basic.urls", namespace="blog_basic")),
 ]
